49192550bc23201b23608387c2bcb6bb4e4dd966
Alay Ghosal <alayghosal009@gmail.com>
2025-04-18 15:07:17 +0530

    jusyt commit

---
+++ Config.java
@@ -0,0 +1,16 @@
+ class Repo{
+    
+<<<<<<< HEAD
+    public static void main(String xyz[])
+    {
+            System.out.println("Good Jobg");
+    }
+=======
+
+            public static int return (int x)
+            {
+                return x*100;
+            }
+
+>>>>>>> main
+ }
